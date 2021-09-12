4759bde2108f9bc85d18192862226b548e283028
anar-sixeliyev <sixeliyev.anar7@gmail.com>
2021-09-12 13:35:27 +0400

    emilgotdu commenti

--- src/components/otheroparts/Footer.js
+++ src/components/otheroparts/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import footer from '../styles/footer.css'
-
+import React from "react";
+import footer from "../styles/footer.css";
+// emil gotdu
 const Footer = () => {
-    return (
-        <div style={footer} className="footer " >
-            <h1>this is footer</h1>
-        </div>
-    );
-}
+  return (
+    <div style={footer} className="footer ">
+      <h1> this is footer </h1>{" "}
+    </div>
+  );
+};
 
 export default Footer;
