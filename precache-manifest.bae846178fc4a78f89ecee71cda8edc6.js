aaf2acda64cca2194e4f698434a8290bb9a44340
anar-sixeliyev <60700896+anar-sixeliyev@users.noreply.github.com>
2021-09-23 11:05:36 +0400

    Updates

---
+++ precache-manifest.bae846178fc4a78f89ecee71cda8edc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e97122ebbb6bdd5c05a8594c3a56621",
+    "url": "/TTT/index.html"
+  },
+  {
+    "revision": "29e6af5b32039da8a38e",
+    "url": "/TTT/static/css/2.ccab356c.chunk.css"
+  },
+  {
+    "revision": "8f046fffc72b341fc94b",
+    "url": "/TTT/static/css/main.d42206bf.chunk.css"
+  },
+  {
+    "revision": "29e6af5b32039da8a38e",
+    "url": "/TTT/static/js/2.d90b956f.chunk.js"
+  },
+  {
+    "revision": "1baa02233bbd827e3584e32091a75902",
+    "url": "/TTT/static/js/2.d90b956f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f046fffc72b341fc94b",
+    "url": "/TTT/static/js/main.0d914ec8.chunk.js"
+  },
+  {
+    "revision": "08cbb5bea27c872f2fb5",
+    "url": "/TTT/static/js/runtime-main.84ad185f.js"
+  },
+  {
+    "revision": "e6b43377ba726388f53fda16eed34176",
+    "url": "/TTT/static/media/logo.e6b43377.png"
+  },
+  {
+    "revision": "a325bcd7e2ec7ab6f3d3eefe4aea8e03",
+    "url": "/TTT/static/media/paperVer1.a325bcd7.jpg"
+  }
+]);
